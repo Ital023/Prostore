bcaeb47c105879389dfa60dfa8674a0b6d23718c
Ital023 <contadoitalo911@gmail.com>
2025-10-21 22:26:29 -0300

    feat: Product type zod

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
+import { Product } from "@/types";
 
 const HomePage = async () => {
-  const latestProducts = await getLatestProducts();
+  const latestProducts: Product[] = await getLatestProducts();
   return <>
     <ProductList data={latestProducts} title="Newest Arrivals"/>
   
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -12,5 +12,9 @@ export async function getLatestProducts() {
     orderBy: { createdAt: "desc" }
   })
 
-  return convertToPlainObject(data);
+  return data.map((product) => ({
+    ...product,
+    price: product.price.toString(),
+    rating: product.rating.toString()
+  }))
 }
