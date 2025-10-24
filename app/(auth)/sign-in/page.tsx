9b13bbec8be514db0b00fe85789a53a79c13698f
Ital023 <contadoitalo911@gmail.com>
2025-10-23 22:38:32 -0300

    feat: layout sign in finished

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,6 +3,7 @@ import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -23,12 +24,12 @@ const SignInPage = () => {
             />
           </Link>
         </CardHeader>
-        <CardTitle className="text-center">Sign In</CardTitle>
+        <CardTitle className="text-center mb-4">Sign In</CardTitle>
         <CardDescription className="text-center">
           Sign in to your account
         </CardDescription>
         <CardContent className="space-y-4">
-          
+          <CredentialsSignInForm />
         </CardContent>
       </Card>
     </div>
