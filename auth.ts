45cb19625f8ee5a7b03b965e44b7da03f6e368dc
Ital023 <contadoitalo911@gmail.com>
2025-10-31 22:18:42 -0300

    feat: customizing jwt token adding role and name in it

--- auth.ts
+++ auth.ts
@@ -51,12 +51,30 @@ export const config = {
   callbacks: {
     async session({ session, token, user, trigger }: any) {
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       if (trigger === "update") {
         session.user.name = user.name;
       }
 
       return session;
+    },
+    async jwt({ token, user, trigger, session: any }) {
+      if (user) {
+        // @ts-ignore
+        token.role = user.role;
+
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
